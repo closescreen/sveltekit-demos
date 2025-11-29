eee3bc897c7c62d48d7e97dbc208a5b8510b3c2a
closescreen <close.screen@gmail.com>
2025-11-30 00:28:48 +0300

    Колесо фортуны и Варианты поместил в меню

--- src/routes/Menu.svelte
+++ src/routes/Menu.svelte
@@ -1,5 +1,4 @@
 <ul tabindex="-1" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-	<li><a>Item 1</a></li>
-	<li><a>Item 2</a></li>
-	<li><a>Item 3</a></li>
+	<li><a href="/fortune-wheel">Колесо фортуны</a></li>
+	<li><a href="/variants/1">Варианты</a></li>
 </ul>
